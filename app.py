6135edee1e45cfb6b7a9d0f8697a67dcb0a26ea1
Emmanuelle Gouillart <emma@plot.ly>
2020-02-26 23:26:43 -0800

    bug

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
+import pandas
 import plotly.graph_objects as go
 import plotly.express as px
 from skimage import data, transform
