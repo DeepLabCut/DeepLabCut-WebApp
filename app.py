c3dfca9c531b634824ff8642c7ad6d6498e5e152
jeylau <30733203+jeylau@users.noreply.github.com>
2020-06-04 11:58:45 +0200

    A couple of figure enhancements

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import base64
+import os
 import random
 import plotly.graph_objects as go
 import plotly.express as px
@@ -12,16 +14,21 @@ KEYPOINTS = ['Nose', 'L_Eye', 'R_Eye', 'L_Ear', 'R_Ear', 'Throat',
              'Withers', 'TailSet', 'L_F_Paw', 'R_F_Paw', 'L_F_Wrist',
              'R_F_Wrist', 'L_F_Elbow', 'R_F_Elbow', 'L_B_Paw', 'R_B_Paw',
              'L_B_Hock', 'R_B_Hock', 'L_B_Stiffle', 'R_B_Stiffle']
+IMAGE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'full_dog.png')
+
 
 img = data.chelsea()
 img = img[::2, ::2]
 images = [img, img[::-1], transform.rotate(img, 30)]
+encoded_image = base64.b64encode(open(IMAGE_PATH, 'rb').read())
 
 
 def make_figure_image(i):
     fig = px.imshow(images[i % len(images)])
+    fig.layout.xaxis.showticklabels = False
+    fig.layout.yaxis.showticklabels = False
     fig.update_traces(hoverinfo='none', hovertemplate='')
-    fig.add_trace(go.Scatter(x=[], y=[], marker_color=[],
+    fig.add_trace(go.Scatter(x=[], y=[], marker_color=[], text=[], hoverinfo=[],
                              marker_cmin=0, marker_cmax=3, marker_size=18, mode='markers'))
     return fig
 
@@ -36,30 +43,46 @@ server = app.server
 options = random.sample(KEYPOINTS, 3)
 already_labeled = set()
 
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
 
 app.layout = html.Div([
     html.Div([
         dcc.Graph(
             id='basic-interactions',
-            config={'editable':True},
-            figure=fig,
-    )],
-    className="six columns"
+            config={'editable': False},
+            figure=fig)
+    ],
+        className="six columns"
     ),
     html.Div([
         html.H2("Controls"),
         dcc.RadioItems(id='radio',
-            options=[{'label':opt, 'value':opt} for opt in options],
-            value=options[0]
-        ),
+                       options=[{'label':opt, 'value':opt} for opt in options],
+                       value=options[0]
+                       ),
         html.Button('Previous', id='previous'),
         html.Button('Next', id='next'),
         html.Button('Clear', id='clear'),
         dcc.Store(id='store', data=0)
-        ], 
-    className="six columns"
+    ],
+        className="six columns"
     ),
-    ])
+    html.Div([
+        dcc.Markdown("""
+                **Instructions**\n
+                Click on the image to add a keypoint.
+            """),
+        html.Pre(id='click-data', style=styles['pre']),
+        html.Img(src='data:image/png;charset=utf-8;base64,{}'.format(encoded_image))
+    ],
+        className='three columns'),
+]
+)
 
 
 @app.callback(
@@ -77,11 +100,11 @@ def display_click_data(n_clicks_n, n_clicks_p, n_clicks_c, val):
         val = 0
     ctx = dash.callback_context
     button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    global already_labeled
+    already_labeled = set()
+    app.layout['radio'].value = options[0]
     if button_id == 'clear':
         fig = make_figure_image(val)
-        global already_labeled
-        already_labeled = set()
-        # app.layout.children[1].children[1].value = options[0]
         return fig, val
     index = val + 1 if button_id == 'next' else val - 1
     fig = make_figure_image(index)
@@ -103,7 +126,7 @@ def display_click_data(clickData, option):
     already_labeled.add(n_bpt)
     x, y = clickData['points'][0]['x'], clickData['points'][0]['y']
     new_option = options[min(len(options) - 1, n_bpt + 1)]
-    return [{'x':[[x]], 'y':[[y]], "marker.color":[[n_bpt]]}, [1]], new_option
+    return [{'x':[[x]], 'y':[[y]], "marker.color":[[n_bpt]], 'text':[[option]]}, [1]], new_option
 
 
 if __name__ == '__main__':
